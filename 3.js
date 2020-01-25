d373d7e11cc2ef252501c5d8f8eda64e390dcfce
DimasBayu24 <59496615+DimasBayu24@users.noreply.github.com>
2020-01-25 19:21:06 +0700

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,10 @@
+var tamu = 6
+var x = tamu; //cuma ngubah jadi simbol biar enak
+var b = 0; //nyimpan nilai jumlah salaman
+
+while (x>1) {
+        x--;
+        b = b + x;    
+        
+}
+console.log("jumlah yang sudah berjabat tangan adalah " + b + " orang apa bila ada " + tamu + "orang di pesta tersebut");
